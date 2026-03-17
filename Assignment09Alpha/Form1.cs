af49d4eaea6ea4ebc552d086c75d145661306e84
drozdiaq <DROZDI@aquinas.edu>
2026-03-17 08:13:13 -0400

    bread class

--- Assignment09Alpha/Form1.cs
+++ Assignment09Alpha/Form1.cs
@@ -16,5 +16,10 @@ namespace Assignment09Alpha
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
